fb33fc01304f8fa6873d6f98ed04dda5c5f1e79e
mrwei9999 <43103992+mrwei9999@users.noreply.github.com>
2018-09-09 11:48:56 +0800

    Add files via upload

---
+++ 1-4.py
@@ -0,0 +1,7 @@
+num = 1
+while num <= 5:
+    print('往右邊搖')
+    print('')
+    print('往左邊搖')
+    print('')
+    num +=1
---
+++ 1-5.py
@@ -0,0 +1,8 @@
+def Choice(message):
+    if message =="往左":
+        print("往左")
+    elif message =="往右":
+        print("往右")
+    else:
+        print('i dont know~')
+Choice("往左")
