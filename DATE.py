8ed7503a475f674b17aa5363e191df2222ff9368
mrwei9999 <43103992+mrwei9999@users.noreply.github.com>
2018-09-29 14:51:27 +0800

    Add files via upload

---
+++ DATE.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+a = input("輸入日期1:(ex:2000/01/01))")
+b = input("輸入日期2:(ex:2000/01/01))")
+x = datetime.strptime(a, '%Y/%m/%d')
+y = datetime.strptime(b, '%Y/%m/%d')
+z = x-y
+print(abs(z.days),'天'')
