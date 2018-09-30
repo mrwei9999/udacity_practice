dbd9838e4f8e6d440f23acddb6be9642fbfdd8bc
mrwei9999 <43103992+mrwei9999@users.noreply.github.com>
2018-09-30 10:39:19 +0800

    Add files via upload

--- secretMessage.py
+++ secretMessage.py
@@ -1,9 +1,15 @@
 import os
-def rename():
+def do_rename():
         file_list = os.listdir(r'C:\Users\capta\Desktop\PYTHON FILES\4fun2\prank\prank')
+        #print(file_list)
         saved_path = os.getcwd()
+        #print(saved_path)
         os.chdir(r'C:\Users\capta\Desktop\PYTHON FILES\4fun2\prank\prank')
         for file_name in file_list:
+            print(file_list)
+            print(file_name)
+            os.chdir(r'C:\Users\capta\Desktop\PYTHON FILES\fun2\prank\prank')
             os.rename(file_name, file_name.translate('0123456789'))
+        
         os.chdir(saved_path)
-rename()
+do_rename()
